9c14764c570b5691299d0e30cfebf4309baac1fd
= <=>
2020-09-01 14:30:13 +0700

    '1'

--- Test/views.py
+++ Test/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
-	msg = EmailMessage(
-	    'Subject here',
-	    'body text go here...',
+	send_mail(
+	    'Tieu de 1',
+	    'Test',
+	    'annakimsohappy@gmail.com',
+	    ['lavahiw670@inxto.net'],
+	    fail_silently=False,
+	)
+	return HttpResponse('success sending')
+
+def email2(request):
+	email = EmailMessage(
+	    'Hello',
+	    'Body goes here',
 	    'annakimsohappy@gmail.com',
 	    ['lavahiw670@inxto.net'],
 	)
-	msg.send()
-	return HttpResponse('success sending')
+	email.send()
+	return HttpResponse('success')
